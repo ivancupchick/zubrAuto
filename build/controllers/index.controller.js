581453824424c581c25da0eb5d9dcd40219fda52
Ivan Kupchyk <ivan.kupchic@gmail.com>
2021-09-26 00:33:51 +0300

    Deleted build

--- build/controllers/index.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexWelcome = void 0;
-function indexWelcome(req, res) {
-    res.sendFile(process.cwd() + "/dist/zubr-auto/"); // check path
-}
-exports.indexWelcome = indexWelcome;
