d71e020a04cb260ee5242c30b6fe255e6b7fa15e
Ivan Kupchyk <ivan.kupchic@gmail.com>
2021-09-26 02:25:28 +0300

    Modified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './build/index.js',
   mode: 'production',
   target: 'node',
+  plugins: [
+    new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })
+  ],
   output: {
     path: path.resolve(__dirname, './build'),
     filename: 'index.bundle.js'
