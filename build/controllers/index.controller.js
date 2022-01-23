ddb6705c2982e81d99cc402478a6f2cf011001ca
Ivan Kupchyk <ivan.kupchic@gmail.com>
2022-01-24 02:09:40 +0300

    Built UI

--- build/controllers/index.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexWelcome = void 0;
-function indexWelcome(req, res) {
-    res.sendFile(process.cwd() + "build/ui/zubr-auto/index.html");
-}
-exports.indexWelcome = indexWelcome;
-//# sourceMappingURL=index.controller.js.map
