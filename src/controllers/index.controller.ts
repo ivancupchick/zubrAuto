814a5bc41a5fbc7d9f06d3465f3253c527877fb7
Ivan Kupchyk <ivan.kupchic@gmail.com>
2022-01-24 02:04:41 +0300

    efd

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
 
 export function indexWelcome(req: Request, res: Response): void {
-   res.sendFile(process.cwd()+"build/ui/zubr-auto/index.html")
+   res.sendFile(process.cwd()+"/build/ui/zubr-auto/index.html")
 }
