91167ba25960334a87437d6f68c10026d55056a4
Ivan Kupchyk <ivan.kupchic@gmail.com>
2024-05-18 18:19:07 +0300

    feat(tokens): extended expriring access token

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -8,7 +8,7 @@ import userTokenRepository from '../repositories/base/user-token.repository';
 
 class TokenService {
   generateTokens(payload: object) {
-    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' });
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '1d' });
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' });
     return {
       accessToken,
