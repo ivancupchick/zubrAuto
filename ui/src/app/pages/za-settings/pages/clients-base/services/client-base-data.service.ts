bae4b75c0586b061b3950890c352ebcf86a11ab9
AndrewNovik <vitalevich16@gmail.com>
2024-09-25 15:40:56 +0300

    UpgradeCarsBase #2

--- ui/src/app/pages/za-settings/pages/clients-base/services/client-base-data.service.ts
+++ ui/src/app/pages/za-settings/pages/clients-base/services/client-base-data.service.ts
@@ -1,6 +1,16 @@
 import { inject, Injectable, OnDestroy } from '@angular/core';
 import { PageagleGridService } from '../../../shared/pageagle-grid/pageagle-grid.service';
-import { BehaviorSubject, Observable, Subject, finalize, map, mergeMap, of, takeUntil, zip } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  Subject,
+  finalize,
+  map,
+  mergeMap,
+  of,
+  takeUntil,
+  zip,
+} from 'rxjs';
 import { BaseList, StringHash } from 'src/app/entities/constants';
 import { RequestService } from 'src/app/services/request/request.service';
 import { environment } from 'src/environments/environment';
@@ -16,58 +26,71 @@ type ClientBaseFilters = {
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ClientBaseDataService extends PageagleGridService<ServerClient.Response> implements OnDestroy {
+export class ClientBaseDataService
+  extends PageagleGridService<ServerClient.Response>
+  implements OnDestroy
+{
   private payload: ClientBaseFilters = {
     page: 1,
-    size: 10
+    size: 10,
   };
 
   private loading = new BehaviorSubject<boolean>(true);
   public loading$ = this.loading.asObservable();
 
-  public clientBaseItems = new BehaviorSubject<BaseList<ServerClient.Response>>({ list: [], total: 0 });
+  public clientBaseItems = new BehaviorSubject<BaseList<ServerClient.Response>>(
+    { list: [], total: 0 },
+  );
   public list$ = this.clientBaseItems.asObservable();
 
   private clientCarsSubject = new BehaviorSubject<ServerCar.Response[]>([]);
   public clientCars$ = this.clientCarsSubject.asObservable();
 
   private destroy$ = new Subject();
-  private carService = inject(CarService)
+  private carService = inject(CarService);
 
-  constructor(
-    private requestService: RequestService
-  ) { super() }
+  constructor(private requestService: RequestService) {
+    super();
+  }
 
   public fetchData(): void {
     this.loading.next(true);
-    this.requestService.get<BaseList<ServerClient.Response>>(`${environment.serverUrl}/${API}`, this.payload)
+    this.requestService
+      .get<BaseList<ServerClient.Response>>(
+        `${environment.serverUrl}/${API}`,
+        this.payload,
+      )
       .pipe(
         mergeMap((clientRes) => {
-          const carIds = clientRes.list.reduce<number[]>((prev, client) => {
-            const clietnCarIds = client.carIds.split(',').map(id => +id);
-            return [...prev, ...clietnCarIds];
-          }, []).filter(id => id && !Number.isNaN(id));
+          const carIds = clientRes.list
+            .reduce<number[]>((prev, client) => {
+              const clietnCarIds = client.carIds.split(',').map((id) => +id);
+              return [...prev, ...clietnCarIds];
+            }, [])
+            .filter((id) => id && !Number.isNaN(id));
 
           if (carIds.length === 0) {
-            return zip(of(clientRes), of({
-              list: [],
-              total: 0
-            } satisfies BaseList<ServerCar.Response>));
+            return zip(
+              of(clientRes),
+              of({
+                list: [],
+                total: 0,
+              } satisfies BaseList<ServerCar.Response>),
+            );
           }
 
-          const query: StringHash = { id: [...(new Set(carIds))].join(',') };
+          const query: StringHash = { id: [...new Set(carIds)].join(',') };
 
           return zip(of(clientRes), this.carService.getCarsByQuery(query));
         }),
         takeUntil(this.destroy$),
-        finalize(() => this.loading.next(false))
+        finalize(() => this.loading.next(false)),
       )
       .subscribe(([clientRes, carsRes]) => {
         this.clientBaseItems.next(clientRes);
         this.clientCarsSubject.next(carsRes?.list || []);
-
       });
   }
 
@@ -79,37 +102,38 @@ export class ClientBaseDataService extends PageagleGridService<ServerClient.Resp
     };
 
     if (this.payload.sortField && this.payload.sortOrder) {
-      [
-        payload.sortField,
-        payload.sortOrder
-      ] = [
+      [payload.sortField, payload.sortOrder] = [
         this.payload.sortField,
-        this.payload.sortOrder
+        this.payload.sortOrder,
       ];
     }
 
     this.payload = payload;
 
     this.fetchData();
-  };
+  }
 
   getClients(): Observable<BaseList<ServerClient.Response>> {
-    return this.requestService.get<BaseList<ServerClient.Response>>(`${environment.serverUrl}/${API}`);
+    return this.requestService.get<BaseList<ServerClient.Response>>(
+      `${environment.serverUrl}/${API}`,
+    );
   }
 
   deleteClient(id: number): Observable<boolean> {
     this.loading.next(true);
-    return this.requestService.delete<any>(`${environment.serverUrl}/${API}/${id}`)
+    return this.requestService
+      .delete<any>(`${environment.serverUrl}/${API}/${id}`)
       .pipe(
         takeUntil(this.destroy$),
         finalize(() => this.loading.next(false)),
-        map(result => {
-        console.log(result);
+        map((result) => {
+          console.log(result);
 
-        return true;
-      }))
-  };
-  
+          return true;
+        }),
+      );
+  }
+
   ngOnDestroy(): void {
     this.destroy$.next(null);
   }
